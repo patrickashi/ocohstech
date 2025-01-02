2a6a15e2df51abd6ea7f644c45628c9ba59551ac
Patrick Ashi <patrickpeko1@gmail.com>
2025-01-02 09:48:21 +0100

    new

--- dashboard_project/dashboard_project/settings.py
+++ dashboard_project/dashboard_project/settings.py
@@ -222,10 +222,10 @@ LOGGING = {
 ALLOWED_HOSTS = ['*']
 
 # for production
-# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
 # for console
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+# EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
