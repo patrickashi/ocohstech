310a17da3a79b7bf6e09f7968c6bf3e6b9b51638
Patrick Ashi <patrickpeko1@gmail.com>
2025-01-23 11:40:14 +0100

    new

--- dashboard_project/wsgi.py
+++ dashboard_project/wsgi.py
@@ -1,4 +1,5 @@
 import os
+print("DJANGO_SETTINGS_MODULE:", os.environ.get('DJANGO_SETTINGS_MODULE'))
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dashboard_project.settings')
