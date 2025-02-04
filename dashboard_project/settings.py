f198feabe5b9ff9da6f41faaeedab0363c84c431
Patrick Ashi <patrickpeko1@gmail.com>
2025-02-05 00:52:50 +0100

    database

--- dashboard_project/settings.py
+++ dashboard_project/settings.py
@@ -112,13 +112,16 @@ CHANNEL_LAYERS = {
 
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
-
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(default=os.getenv('DATABASE_URL'))
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 # DATABASE_URL = os.getenv('DATABASE_URL')
 
