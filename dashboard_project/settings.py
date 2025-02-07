b18d280a3c7d2675893ed0a6c5bb03637e3525ee
Patrick Ashi <patrickpeko1@gmail.com>
2025-02-07 10:10:37 +0100

    new

--- dashboard_project/settings.py
+++ dashboard_project/settings.py
@@ -112,6 +112,7 @@ CHANNEL_LAYERS = {
 
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
+
 DATABASES = {
     'default': dj_database_url.config(default=os.getenv('DATABASE_URL'))
 }
